ef0209cdcd6a9bfdfc83e059d20e37db9e5b1df4
J Seymour <JustJakeSeymour@gmail.com>
2023-02-01 15:50:34 -0700

    create service and facade specs for movies

---
+++ spec/services/the_movie_db_service_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe TheMovieDbService do
+  describe "class methods" do
+    describe "#get_movie_data" do
+      it "returns movie data" do
+        
+      end
+    end
+  end
+end
