dce219f573bf4d1e6fbf4bace74d15e322a92394
J Seymour <JustJakeSeymour@gmail.com>
2023-02-01 17:49:22 -0700

    add additional test for link to movie show

--- spec/features/users/movies/index_spec.rb
+++ spec/features/users/movies/index_spec.rb
@@ -9,11 +9,21 @@ RSpec.describe "Movies Results (Index) Page" do
   describe "display movies from top rated" do
     it "happy path" do
       click_button("Find Top Rated Movies")
-
+      
       expect(current_path).to eq("/users/#{@user.id}/movies")
       
       expect(page.status_code).to eq 200
       expect(page).to have_content("The Godfather")
+    end
+    
+    it "links to movie show" do
+      click_button("Find Top Rated Movies")
+
+      expect(page).to have_link("The Godfather")
+
+      click_link("The Godfather")
+
+      expect(current_path).to eq("/users/#{@user.id}/movies/238")
     end
   end
   
