0f74d5473e72314b1c264063482712f245a82923
J Seymour <JustJakeSeymour@gmail.com>
2023-02-01 17:16:14 -0700

    remove a redundant route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :users, only: [:show, :create] do
     resources :discover, only: [:index], controller: "users/discover"
     post "/movies", to: "users/movies#index"
-    post "/movies", to: "users/movies#keyword"
     resources :movies, only: [:index, :show], controller: "users/movies"
   end
 end
